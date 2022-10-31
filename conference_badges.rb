7bf4727c6e4b4932db62983854b82477b46358f9
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-10-31 16:07:16 -0400

    commit tree abeb58df53dcc56dcaf2e63f8785ee7e59360805

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker speaker
+    "Hello, my name is #{speaker}."
+end
+
+def batch_badge_creator new_array
+    new_array.map do |speaker|
+        "Hello, my name is #{speaker}."
+    end
+end
+
+def assign_rooms speakers
+    speakers.map.with_index(1) do |speaker, room_number|
+        "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+def printer speakers
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+    
+    assign_rooms(speakers).each do |assignment|
+        puts assignment
+    end
+end
